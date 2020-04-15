e81d1c1e52b4ac87df0861af6779d07d7e30b238
hpal <ahosea15@gmail.com>
2020-04-15 05:04:28 +0300

    change url

--- src/services/events.js
+++ src/services/events.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export function getEvents() {
-    return axios.get('https://my.api.mockaroo.com/events.json?key=9970b2f0').then(
+    return axios.get('https://my.api.mockaroo.com/events?key=9970b2f0').then(
         response => { return response.data }
     )
 }
